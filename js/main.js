6cca71be1b875f3688e466a4c5fb8a8b9a6c7359
AzizbekQuvatov2009 <quvatovazizbek22@gmail.com>
2023-05-20 11:01:32 +0500

    JS_1-dars kodlari
    JS_1-dars kodlari yuklandi

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*
+console.log(4+4*2/5+20-3);
+
+alert("Sayitimizga xush kelibsiz!")
+
+document.write("Men bravzerning body qismidanman!")*/
+
+/*
+let x = 5;
+let y = 7;
+x = 12;
+*/
+/*let salom = "Assalomu aleykum!";*/
+/*let x = 5, y = 12, z = 15;
+document.write(x + y);*/
+
+/* Data type (Ma'lumatlat turi:number,string) */
+
+let son =15;
+let matn = "Hello world!";
+
+document.write(typeof matn);
+
